2350493fddd6571f73d37b1145537fb6a1c88a9b
Soumyadeep Dey <161801065+Soumyadeep-Dey10@users.noreply.github.com>
2025-05-22 23:38:29 +0530

    Update Stock Prices.java

--- Array/Stock Prices.java
+++ Array/Stock Prices.java
@@ -1,11 +1,13 @@
+//https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/stock-buy-and-sell2615
 class Solution {
     public int maximumProfit(int prices[]) {
-        int profit = 0;
+        // code here
+        int res = 0;
         for(int i = 1; i < prices.length; i++){
             if(prices[i] > prices[i-1]){
-                profit += prices[i] - prices[i-1];
+                res += prices[i] - prices[i-1];
             }
         }
-        return profit;
+        return res;
     }
 }
