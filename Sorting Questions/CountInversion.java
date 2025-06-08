ab74e9a5e9466a4548f0a01ce6c6e27f590c49d9
Soumyadeep Dey <161801065+Soumyadeep-Dey10@users.noreply.github.com>
2025-06-08 12:53:51 +0530

    Add files via upload

---
+++ Sorting Questions/CountInversion.java
@@ -0,0 +1,47 @@
+// https://www.geeksforgeeks.org/batch/gfg-160-problems/track/sorting-gfg-160/problem/inversion-of-array-1587115620
+class Solution {
+    // Function to count inversions in the array.
+       private static int merge(int[] arr, int low, int mid, int high){
+            ArrayList<Integer> list = new ArrayList<>();
+            int left = low;
+            int right = mid+1;
+            int count = 0;
+            
+            while(left <= mid && right <= high){
+                if(arr[left] <= arr[right]){
+                    list.add(arr[left]);
+                    left++;
+                }else{
+                    list.add(arr[right]);
+                    count += (mid - left+1);
+                    right++;
+                }
+            }
+            while(left <= mid){
+                list.add(arr[left]);
+                left++;
+            }
+            while(right <= high){
+                list.add(arr[right]);
+                right++;
+            }
+            for(int i = low; i <= high; i++){
+                arr[i] = list.get(i-low);
+            }
+            
+            return count;
+        }
+        // Your Code Here
+        public static int mergeSort(int[] arr, int low, int high){
+            int count = 0;
+            if(low >= high) return count;
+            int mid = (low+high)/2;
+            count += mergeSort(arr, low, mid);
+            count += mergeSort(arr, mid+1,high);
+            count += merge(arr, low, mid, high);
+            return count;
+        }
+    static int inversionCount(int arr[]) {
+        return mergeSort(arr, 0,arr.length-1);
+    }
+}
