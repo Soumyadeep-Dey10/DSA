19ecb03bb552666e7108f4e58d7fde822693b82c
Soumyadeep Dey <161801065+Soumyadeep-Dey10@users.noreply.github.com>
2025-06-30 13:27:27 +0530

    Update Max Sub Subarray.java

--- Sliding Window/Max Sub Subarray.java
+++ Sliding Window/Max Sub Subarray.java
@@ -1,22 +1,21 @@
 //https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1
 class Solution {
-    public int maximumSumSubarray(int[] nums, int k) {
+    public int maximumSumSubarray(int[] arr, int k) {
         // Code here
         int i = 0, j = 0;
-        int sum = 0, maxSum = Integer.MIN_VALUE; 
-        
-        while(j < nums.length){
-            sum += nums[j];  
+        int sum = 0, maxSum = Integer.MIN_VALUE;
+        while(j < arr.length){
+            sum += arr[j];
             
-            if(j - i + 1 == k) { 
-                maxSum = Math.max(sum, maxSum);  
-                sum -= nums[i];  
-                i++;  
+            if(j - i + 1 < k) j++;
+            
+            else if(j - i + 1 == k){
+                maxSum = Math.max(maxSum, sum);
+                sum  = sum - arr[i];
+                i++;
+                j++;
             }
-            
-            j++; 
         }
-        
-        return maxSum; 
+        return maxSum;
     }
 }
