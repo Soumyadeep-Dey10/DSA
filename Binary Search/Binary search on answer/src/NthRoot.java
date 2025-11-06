b862848d7ef630653d9b17462c46b3c9c5a7e42f
Soumyadeep Dey <161801065+Soumyadeep-Dey10@users.noreply.github.com>
2025-11-06 20:01:41 +0530

    Update NthRoot.java

--- Binary Search/Binary search on answer/src/NthRoot.java
+++ Binary Search/Binary search on answer/src/NthRoot.java
@@ -1,9 +1,26 @@
-public class NthRoot {
-    public static int findRoot(int number, int m){
-        int low = 1, high = number;
-
-    }
-    public static void main(String[] args) {
-
+//https://www.geeksforgeeks.org/problems/find-nth-root-of-m5843/1
+class Solution {
+    
+    int calculate(int mid, int n, int m){
+        long ans = 1;
+        for(int i = 1; i <= n; i++){
+            ans = ans * mid;
+            if(ans > m) return 2; //too large
+        }
+        if(ans == m) return 1; //correct
+        return 0;
+    } 
+    public int nthRoot(int n, int m) {
+        // code here
+        int low = 1, high = m;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            
+            int res = calculate(mid, n, m);
+            if(res == 1) return mid;
+            else if(res == 2) high = mid-1;
+            else low = mid+1;
+        }
+        return -1;
     }
 }
