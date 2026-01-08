770ef98035c47412e25021b82ae1f70d8b7fd05b
Soumyadeep Dey <161801065+Soumyadeep-Dey10@users.noreply.github.com>
2026-01-08 20:42:17 +0530

    Add files via upload

---
+++ Imp DSA Patterns/Merge Overlapping Subintervals/OverlappingIntervals.java
@@ -0,0 +1,30 @@
+class Solution {
+    static boolean isIntersect(int[][] a) {
+        // Code Here
+        Arrays.sort(a, (x, y) -> x[0] - y[0]);
+
+        int n = a.length;
+        
+        int idx = 0;
+
+        int s1 = a[0][0];
+        int e1 = a[0][1];
+
+        for (int i = 1; i < n; i++) {
+            int s2 = a[i][0];
+            int e2 = a[i][1];
+
+            // overlapping case
+            if (e1 >= s2) {
+                return true;
+            }
+            // not overlapping
+            else {
+                s1 = s2;
+                e1 = Math.max(e1,e2);
+            }
+        }
+
+        return false;
+    }
+}
