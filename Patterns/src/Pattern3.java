2a250d21db08035ed01d56a986d9c036df9f10d9
Soumyadeep Dey <161801065+Soumyadeep-Dey10@users.noreply.github.com>
2026-01-13 20:43:09 +0530

    Update Pattern3.java

--- Patterns/src/Pattern3.java
+++ Patterns/src/Pattern3.java
@@ -1,25 +1,34 @@
-import java.util.Scanner;
-
-//    *
-//   **
-//  ***
-// ****
-//*****
-
-public class Pattern3 {
+// "static void main" must be defined in a public class.
+					*	
+				*	*	
+			*	*	*	
+		*	*	*	*	
+	*	*	*	*	*	
+public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the value of n :");
-        int n = sc.nextInt();
-
-        for (int i = 1; i <= n; i++){
-            for (int k = 1; k <= n - i; k++){
-                System.out.print(" ");
+        for(int i = 1; i <= 5; i++){
+            for(int k = 5-i; k >= 0; k--){
+                System.out.print("\t");
             }
-            for (int j = 1; j <= i; j++){
-                System.out.print("*");
+            for(int j = 1; j <= i; j++){
+                System.out.print("*\t");
             }
-            System.out.println();
+             System.out.println();
         }
+       
     }
+    //second approach
+    public static void main(String[] args) {
+        int sp = 4, st = 1;
+        for(int i = 1; i <= 5; i++){
+            for(int k = 1; k <= sp; k++){
+                System.out.print("\t");
+            }
+            for(int j = 1; j <= st; j++){
+                System.out.print("*\t");
+            }
+             System.out.println();
+             sp--;
+             st++;
+        }
 }
