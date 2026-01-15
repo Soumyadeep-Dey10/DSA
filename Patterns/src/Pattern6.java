9f42778386a2bcfe7be2d7e46d27140a362a160a
Soumyadeep Dey <161801065+Soumyadeep-Dey10@users.noreply.github.com>
2026-01-15 20:34:27 +0530

    Add files via upload

---
+++ Patterns/src/Pattern6.java
@@ -0,0 +1,38 @@
+// *	*	*		*	*	*	
+// *	*				*	*	
+// *						*	
+// *	*				*	*	
+// *	*	*		*	*	*
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 5;
+        int st = n/2 + 1, sp = 1;
+        
+        for(int i = 1; i <= n; i++){
+            
+           for(int k = 1; k <= st; k++){
+                System.out.print("*\t");
+            }
+            
+            for(int j = 1; j <= sp; j++){
+                System.out.print("\t");
+            }
+            
+            for(int k = 1; k <= st; k++){
+                System.out.print("*\t");
+            }
+            
+            if(i <= n/2){
+                st--;
+                sp += 2;
+            }
+            else{
+                st++;
+                sp -= 2;
+            }
+             System.out.println(); 
+        }
+       
+    }
+}
