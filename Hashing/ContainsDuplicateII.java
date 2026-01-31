32ff4e93d3dc884420ebaf0f51c339131944de7c
Soumyadeep Dey <161801065+Soumyadeep-Dey10@users.noreply.github.com>
2026-01-31 21:04:29 +0530

    Add files via upload

---
+++ Hashing/ContainsDuplicateII.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/contains-duplicate-ii/?envType=problem-list-v2&envId=hash-table
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                if(i - map.get(nums[i]) <= k)return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
